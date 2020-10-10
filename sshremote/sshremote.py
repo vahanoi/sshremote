019843f1977019efcc219369fe640a29884e161e
vahanoi <vahanoi@gmail.com>
2020-10-10 19:20:55 +0100

    SSH client object fixes

--- sshremote/sshremote.py
+++ sshremote/sshremote.py
@@ -8,22 +8,30 @@ import paramiko  # import SSH2 library
 import os
 
 class sshclient:
-    
-    def __init__(self,command):
-        self.ssh_client = sshclient
-        self.command = command
+    '''SSH client class - '''
+    def __init__(self,host):  # ssh_host - list: hostname/IP, username, password, command/commands
+        self.ssh_client = host
 
-    def ssh_session(self, hostname, username, password):
-        ssh_host = [hostname, username, password]
-
+    def ssh_session(self, ssh_host):
+        
         self.ssh_client = paramiko.SSHClient()
         self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.ssh_client.connect(hostname=ssh_host[1],
-                        username='username',
-                        password='password')
+        self.ssh_client.connect(hostname=ssh_host[0],
+                        username=ssh_host[1],
+                        password=ssh_host[2])
         stdin, stdout, stderr = self.ssh_client.exec_command(self.command)
         print(stdout.read().decode().strip())
         print(stderr.read().decode().strip())
+        print("Command: "+stdin.read().decode().strip())
         self.ssh_client.close()
-# help
+
+def main ():
+    ssh_host = ['123.123.123.123', 'test','testtest','ls -l']
+    ssh = sshclient(ssh_host)
+    ssh.ssh_session(ssh_host)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+# help with the programming
 # test
